01fb45754b33005b79d517b118956476807e87fd
Edward Tam <edwardkt@usc.edu>
2014-08-10 07:03:30 +0000

    listed where to put aws keys in knife config file

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,4 +1,4 @@
-knife[:aws_access_key_id] = ''
-knife[:aws_secret_access_key] = ''
+knife[:aws_access_key_id] = 'type_your_aws_access_key_here'
+knife[:aws_secret_access_key] = 'type_your_aws_secret_access_key_here'
 knife[:aws_ssh_key_id] = 'chef-test'
 knife[:chef_mode] = 'solo'
